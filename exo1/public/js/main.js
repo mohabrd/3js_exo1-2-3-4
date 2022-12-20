62f9be3ecccd70a3ed550f4faea56acf1b4def4f
mohabrd <bezrhoudimohamed@gmail.com>
2022-12-20 14:41:46 +0100

    exo1 fini

---
+++ exo1/public/js/main.js
@@ -0,0 +1,20 @@
+let semaine;
+semaine = prompt("Un jour de la semaine")
+switch (semaine)
+{
+    case "lundi":
+        alert("On est lundi. Tu as cours  à MolenGeek et tu as sport en soirée.");
+        break;
+    case "mardi":
+        alert("On est mardi. Tu as cours  à MolenGeek et tu as sport en soirée.");
+        break;
+    case "mercredi":
+        alert("On est mercredi. Tu as cours  à MolenGeek et tu as sport en soirée.");
+        break;
+    case "jeudi":
+        alert("On est jeudi. Tu as cours  à MolenGeek et tu as sport en soirée.");
+        break;
+    case "vendredi":
+        alert("On est vendredi. Tu as cours  à MolenGeek et tu as sport en soirée.");
+        break;
+}
