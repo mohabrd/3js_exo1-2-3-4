aee2e06fa06659e3fb0ff30a4661eb5c37ccea23
mohabrd <bezrhoudimohamed@gmail.com>
2022-12-20 14:47:54 +0100

    exo2 fini

---
+++ exo2/public/js/main.js
@@ -0,0 +1,10 @@
+let meteo = "neige";
+
+switch (meteo) {
+    case "pluie":
+        console.log("Il pleut. Je prend un parapluie");
+        break;
+    case "neige":
+        console.log("Il neige. Je prend une luge");
+        break;
+}
